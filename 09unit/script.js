5da0b84f506f000b0a98ec91b3cfc4939859263f
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2022-03-16 18:29:32 -0600

    Made header work for small screens

--- 09unit/script.js
+++ 09unit/script.js
@@ -1,7 +1,17 @@
+//References to main navigation
 const ul = document.querySelector('.ul')
-const ul__exploreUl = document.querySelector('.ul__exploreUl')
-const hamburgerButton = document.querySelector('.nav__hamburger')
+const hamburgerButton = document.querySelector('.nav__button')
 
+//References to sub navigation
+const ulexplore = document.querySelector('.ul__exploreUl')
+const exploreButton = document.querySelector('.ul__exploreIcon')
+
+//Main nav expanding
 hamburgerButton.addEventListener('click', () => {
-    ul.classList.toggle('ul--open')
+    ul.classList.toggle('ul--closed')
+})
+
+//Sub nav expanding
+exploreButton.addEventListener('click', ()=> {
+    ulexplore.classList.toggle('ul__exploreUl--closed')
 })
