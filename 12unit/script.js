09db2e4bd6fa094dac319dc162b62f2958a9e7fc
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2022-04-12 18:15:29 -0600

    added DOM elements through javascript

--- 12unit/script.js
+++ 12unit/script.js
@@ -6,19 +6,60 @@ fetch(hotelData)
     console.log(myList)
 
     myList.forEach(hotel => {
+    //css grid 
+    let inventoryWrapper = document.querySelector('.main__inventoryWrapper')
+    
+    //grid items
+    let figureDiv = document.createElement('div')
+    figureDiv.className = 'main__figureDiv'
 
+    //Figure items
     let myImageTag = document.createElement('img')
+    myImageTag.className = 'main__figureImg'
     myImageTag.src = hotel.photo
 
     let myCaptionTag = document.createElement('figcaption')
+    myCaptionTag.className = 'main__figcaption'
     myCaptionTag.textContent = hotel.name
 
     let myFigureTag = document.createElement('figure')
+    myFigureTag.className = 'main__figure'
+
+    //Contact Items
+    let addressPhoneDiv = document.createElement('div')
+    addressPhoneDiv.className = '.main__addressPhoneDiv'
+
+    //Address grid div
+    let addressIcon = document.createElement('div')
+    addressIcon.innerHTML = `<ion-icon name="map"></ion-icon>`
+    addressIcon.className = 'main__addressIcon'
+
+    let phoneIcon = document.createElement('div')
+    phoneIcon.innerHTML = `<ion-icon name="call"></ion-icon>`
+    phoneIcon.className = 'main__phoneIcon'
+
+    let address = document.createElement('p')
+    address.className = 'main__address'
+    address.innerHTML = `${hotel.address[0]} <br> ${hotel.address[1]}`
+
+    let phone = document.createElement('p')
+    phone.className = 'main__phone'
+    phone.textContent = hotel.phone
+
+
     myFigureTag.appendChild(myImageTag)
     myFigureTag.appendChild(myCaptionTag)
-    document.querySelector('.inventoryWrapper').appendChild(myFigureTag)
+    figureDiv.appendChild(myFigureTag)
 
-    myFigureTag.className = 'main__figure'
+    addressPhoneDiv.appendChild(addressIcon)
+    addressPhoneDiv.appendChild(address)
+    addressPhoneDiv.appendChild(phoneIcon)
+    addressPhoneDiv.appendChild(phone)
+    
+    inventoryWrapper.appendChild(figureDiv)
+    figureDiv.appendChild(addressPhoneDiv)
+
+    
     })
 })
 
